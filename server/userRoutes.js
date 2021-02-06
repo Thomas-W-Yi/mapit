5210cfed1a175cb97f2b2ce2fbc533be62795475
Borrisson <lucborris@gmail.com>
2021-02-06 18:24:08 +0000

    Add Server route files

---
+++ server/userRoutes.js
@@ -0,0 +1,19 @@
+module.exports = function (router, database) {
+
+
+
+  // EXAMPLE
+  // router.post('/properties', (req, res) => {
+  //   const userId = req.session.userId;
+  //   database.addProperty({...req.body, owner_id: userId})
+  //     .then(property => {
+  //       res.send(property);
+  //     })
+  //     .catch(e => {
+  //       console.error(e);
+  //       res.send(e)
+  //     });
+  // });
+
+return router;
+};
