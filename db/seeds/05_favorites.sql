333d2b8d45890ac0adc8cb7f479b2efc37ed4c61
Borrisson <lucborris@gmail.com>
2021-02-06 20:15:35 +0000

    Add seeds into favorites table in db

---
+++ db/seeds/05_favorites.sql
@@ -0,0 +1,7 @@
+INSERT INTO favorites (user_id, map_id) VALUES (3, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (3, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (4, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (4, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (6, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (7, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (8, 2);
