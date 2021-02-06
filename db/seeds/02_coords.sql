fd968e6439fdca393d5816eaa68a1e5a65a3c3c1
Borrisson <lucborris@gmail.com>
2021-02-06 19:38:55 +0000

    Add seeds for coords Table in db

---
+++ db/seeds/02_coords.sql
@@ -0,0 +1,6 @@
+INSERT INTO coords (latitude, longitude) VALUES (45.468558, -73.741771);
+INSERT INTO coords (latitude, longitude) VALUES (43.678301, -79.628697);
+INSERT INTO coords (latitude, longitude) VALUES (43.744065, -79.468653);
+INSERT INTO coords (latitude, longitude) VALUES (43.627129, -79.397558);
+INSERT INTO coords (latitude, longitude) VALUES (45.520541, -73.411833);
+INSERT INTO coords (latitude, longitude) VALUES (45.676382, -74.044011);
