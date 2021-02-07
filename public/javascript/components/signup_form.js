b5c7044bc84549927cfa7f18214d602885a44948
Borrisson <lucborris@gmail.com>
2021-02-07 15:40:54 +0000

    Add sign up form carbon copy of sign in

--- public/javascript/components/signup_form.js
+++ public/javascript/components/signup_form.js
@@ -1,26 +1,27 @@
 $(() => {
 
   const $signUpForm = $(`
-  <form id="sign-up-form" class="sign-up-form">
-        <p>Sign Up</p>
+  <div class="sign-up sign-in-container">
+  <form class="form-signin">
+    <div class="text-center mb-4">
+      <img class="mb-4" src="" alt="" width="72"
+        height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Map It!</h1>
+    </div>
 
-        <div class="sign-up-form__field-wrapper">
-            <input type="text" name="name" placeholder="Username">
-          </div>
+    <div class="form-label-group">
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputEmail">Email address</label>
+    </div>
 
-        <div class="sign-up-form__field-wrapper">
-          <input type="email" name="email" placeholder="Email">
-        </div>
-
-        <div class="sign-up-form__field-wrapper">
-            <input type="password" name="password" placeholder="Password">
-          </div>
-
-        <div class="sign-up-form__field-wrapper">
-            <button>Sign Up</button>
-            <a id="sign-up-form__cancel" href="#">Cancel</a>
-        </div>
-      </form>
+    <div class="form-label-group">
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <label for="inputPassword">Password</label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
+    <p class="mt-5 mb-3 text-muted text-center">&copy; 2021</p>
+  </form>
+</div>
   `);
   window.$signUpForm = $signUpForm;
 
@@ -35,10 +36,4 @@ $(() => {
         views_manager.show('listings');
       });
   });
-
-  $('body').on('click', '#sign-up-form__cancel', function() {
-    views_manager.show('listings');
-    return false;
-  });
-
 });
