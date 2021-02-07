d1df138141de085fe0b27a89484a9f28414e8ab4
Borrisson <lucborris@gmail.com>
2021-02-07 15:57:16 +0000

    Add main map view for view_manager

---
+++ public/javascript/components/main_map.js
@@ -0,0 +1,10 @@
+$(() => {
+
+  const $mainMap = $(`
+  <div id="mymap"></div>
+  `);
+  window.$signUpForm = $mainMap;
+
+
+  //We can add the event listeners of the MAPS ONLY HERE!!! (Any list related functions should be with list file itself)
+});
