bd8ef7151d0e1cbd990482869a77f583d72893b6
Borrisson <lucborris@gmail.com>
2021-02-07 13:28:40 +0000

    Fix navbar to completely responsive for all device viewport sizes

--- public/javascript/components/header.js
+++ public/javascript/components/header.js
@@ -7,26 +7,29 @@ $(() => {
     currentUser = user;
     $pageHeader.find("#page-header__user-links").remove();
     let userLinks = `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 px-5">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 px-1">
     <div class="container-fluid">
       <a class="navbar-brand" href="#"><img src="images/home-icon-silhouette.png" alt=""></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>`;
-
+  `;
     if (!user) {
       userLinks += `
-        <div class="collapse navbar-collapse" id="navbarColor03">
-          <button id="login-btn"class="btn btn-outline-dark mx-2 my-2 my-sm-0" type="submit">Sign in</button>
-          <button id="register-btn"class="btn btn-light my-2 my-sm-0 text-dark" type="submit">Register</button>
-        </div>
-      </div>
-    </nav>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-outline-dark me-2" type="submit">Login</button>
+        <button class="btn btn-dark" type="submit">Register</button>
+      </form>
+    </div>
+  </div>
+</nav>
       `;
     } else {
       userLinks += `
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             My Profile
