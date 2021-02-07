c2189c00247fe1f99319bd86b550615e54ca32d6
Borrisson <lucborris@gmail.com>
2021-02-07 12:36:50 +0000

    Add work in progress for switching branch

--- public/javascript/components/header.js
+++ public/javascript/components/header.js
@@ -6,17 +6,17 @@ $(() => {
   function updateHeader(user) {
     currentUser = user;
     $pageHeader.find("#page-header__user-links").remove();
-    let userLinks;
+    let userLinks = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 px-5">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="images/home-icon-silhouette.png" alt=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>`;
 
     if (!user) {
-      userLinks = `
-      <nav class="navbar navbar-expand-lg navbar-light p-3">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarColor03">
+      userLinks += `
+        <div class="collapse navbar-collapse" id="navbarColor03">
           <button id="login-btn"class="btn btn-outline-dark mx-2 my-2 my-sm-0" type="submit">Sign in</button>
           <button id="register-btn"class="btn btn-light my-2 my-sm-0 text-dark" type="submit">Register</button>
         </div>
@@ -24,44 +24,31 @@ $(() => {
     </nav>
       `;
     } else {
-      userLinks = `
-      <nav class="navbar navbar-expand-lg navbar-light py-3">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="images/home-icon-silhouette.png" alt=""></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarColor03"
-          aria-controls="navbarColor03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarColor03">
-          <div class="dropdown">
-            <button
-              class="btn btn-outline-dark dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Profile
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <li><a class="dropdown-item" href="#">My Favorites</a></li>
-              <li><a class="dropdown-item" href="#">My Contributions</a></li>
-              <li><a class="dropdown-item" href="#">My Maps</a></li>
-            </ul>
-          </div>
-          <button id="logout-btn" class="btn btn-outline-dark mx-2 my-2 my-sm-0">
-            Logout
-          </button>
-        </div>
-      </div>
-    </nav>
+      userLinks += `
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            My Profile
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">My Favorites</a></li>
+            <li><a class="dropdown-item" href="#">My Contributions</a></li>
+            <li><a class="dropdown-item" href="#">My Maps</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Create Map</a></li>
+          </ul>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Hello ${user.name}</a>
+          </li>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-outline-danger type="submit">Logout</button>
+      </form>
+    </div>
+  </div>
+</nav>
       `;
     }
 
