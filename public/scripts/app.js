d8823addb25c240342635aabfd812e1c1fcf222a
thomaswyee <thomas.w.yee@gmail.com>
2021-02-07 21:16:45 +0000

    update createMap. Add comments and add form to update marker and create new marker

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -30,27 +30,65 @@ $(() => {
   };
 
   const createMap = (map) => {
+    // remove previous map before add new map
+    if (mymap && mymap.remove) {
+      mymap.remove();
+    }
+    $("#map-container").html("");
+    $("#map-container").html('<div id="mymap"></div>');
+
+    // get lat, lng, name, and id from map obj
     const { latitude, longitude, name, id } = map;
+
+    // create mapObj
     const mymap = L.map("mymap").setView([latitude, longitude], 10);
+
+    // fill the map with tiles, we use openstreetmap api
     const attribution = `${name} is created by ${map.user_name}`;
     const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
     L.tileLayer(tileUrl, { attribution }).addTo(mymap);
+
+    // add markers/points to map, and we get points data from our apiRoutes
     getAllPoints(id).then((map_points) => {
       for (const id in map_points) {
         const { latitude, longitude, title, des, marker_url } = map_points[id];
         const myIcon = L.icon({
           iconUrl: `${marker_url}`,
-          iconSize: [38, 95],
+          iconSize: [20, 40],
         });
+        // add individual markers to the map
         L.marker([latitude, longitude], { icon: myIcon })
+          // click event on marker will trigger new form for update/delete for this marker
+          .on("click", clickPoint)
           .addTo(mymap)
           .bindPopup(`<p>${title}<br />${des}.</p>`)
           .openPopup();
       }
     });
+
     mymap.on("click", function (event) {
       const lat = event.latlng.lat;
       const long = event.latlng.lng;
+      // click event on mymap will create new marker to save/discard
+      $("#map-container").append(`<div id = 'save-point'>
+    <form>
+  <div class="form-group">
+    <label>Latitude: ${lat}</label>
+    <label>Longitude: ${long}</label>
+    <label>map_id: ${map.id}</label>
+    <label>map_id: ${map.user_id}</label>
+    <input type="text" class="form-control" id="InputText" aria-describedby="emailHelp" placeholder="Enter Title">
+  </div>
+  <div class="form-group">
+    <input type="text" class="form-control" id="InputDescription" placeholder="Description">
+  </div>
+  <div class="form-group">
+    <input type="text" class="form-control" id="InputImgUrl" placeholder="Img Url">
+  </div>
+  <button type="submit" class="btn btn-primary">Save Point</button>
+  <button type="submit" class="btn btn-warning">Discard</button>
+</form>
+    </div>`);
       L.marker([lat, long])
         .addTo(mymap)
         .bindPopup(`map point lat: ${lat} and long: ${long}`)
