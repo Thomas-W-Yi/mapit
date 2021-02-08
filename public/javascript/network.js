e26066b397ce697567c3b240d6314851298fadef
Borrisson <lucborris@gmail.com>
2021-02-08 13:45:11 +0000

    Add required routes

--- public/javascript/network.js
+++ public/javascript/network.js
@@ -1,3 +1,6 @@
+// --------- GET ROUTES ----------
+
+
 function getMaps(params) {
   let url = "/api/maps";
   if (params) {
@@ -15,5 +18,106 @@ function getMarkersForMap(mapId) {
   });
 }
 
-//test only
-$.when(getMaps(`map_id=${1}`), getMarkersForMap(`map_id=${1}`)).done((data1, data2) => console.log(data1,data2));
+function signUp() {
+  return $.ajax({
+    url: "/users",
+  });
+}
+
+function getMyDetails() {
+  return $.ajax({
+    url: "/users/me",
+  });
+}
+
+
+function logIn() {
+  return $.ajax({
+    url: "/users/login",
+  });
+}
+
+
+
+
+// -------- POST ROUTES ---------
+
+function addMap(data) {
+  return $.ajax({
+    method: 'POST',
+    url:"/api/maps",
+    data,
+  });
+}
+
+function addMarker(data) {
+  return $.ajax({
+    method: 'POST',
+    url:"/api/markers",
+    data,
+  });
+}
+
+function addFavorite(data) {
+  return $.ajax({
+    method: 'POST',
+    url:"/api/favorites",
+    data,
+  });
+}
+
+function signUp(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/users",
+    data
+  });
+}
+
+
+function logIn(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/users/login",
+    data
+  });
+}
+
+function logOut() {
+  return $.ajax({
+    method: "POST",
+    url: "/users/logout",
+  })
+}
+
+
+// -------- PUT ROUTES ---------
+
+function updateMarker(data) {
+  return $.ajax({
+    method: 'PUT',
+    url:"/api/markers",
+    data,
+  });
+}
+
+
+
+
+// -------- DELETE ROUTES ---------
+
+function deleteMarker(data) {
+  return $.ajax({
+    method: 'DELETE',
+    url:"/api/markers",
+    data,
+  });
+}
+
+function deleteFavorite(data) {
+  return $.ajax({
+    method: 'DELETE',
+    url:"/api/favorites",
+    data,
+  });
+}
