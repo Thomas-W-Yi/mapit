80c68c679b54c791185debb8c70507d70557126a
Borrisson <lucborris@gmail.com>
2021-02-10 17:27:07 +0000

    Add event listeners for delete and modify buttons

--- public/javascript/components/modify_marker_form.js
+++ public/javascript/components/modify_marker_form.js
@@ -2,18 +2,51 @@ $(() => {
   const $modifyMarkerForm =
     $(`
     <form id = 'update-marker-frm'>
-  <div class="form-group">
-    <input type="text" name = 'title' class="form-control" id="InputText" placeholder="Enter New Title" required>
-  </div>
-  <div class="form-group">
-    <input type="text" name='description' class="form-control" id="InputDescription" placeholder="Enter New Description" required>
-  </div>
-  <div class="form-group">
-    <input type="url" name = 'img_url' class="form-control" id="InputImgUrl" placeholder="Update img url" required>
-  </div>
+    <input type="hidden" name="map_id" value="">
+    <div class="form-group">
+    <input type="text" placeholder="Latitude" name = 'latitude' class="form-control" id="InputText" value="" readonly required>
+   </div>
+   <div class="form-group">
+    <input type="text" name='longitude' placeholder="Longitude" class="form-control" id="InputText" value="" readonly required>
+   </div>
+   <input type="hidden" name="id" value="">
+    <div class="form-group">
+    <input name="title" type="text" class="form-control" id="InputText"  placeholder="Enter Title" required>
+    </div>
+    <div class="form-group">
+    <input name="description" type="text" class="form-control" id="InputDescription" placeholder="Description" required>
+    </div>
+    <div class="form-group">
+    <input type="url" name="img_url" class="form-control" id="InputImgUrl" placeholder="Img Url" required>
+    </div>
   <button type="button" id="submit-update" class="btn btn-primary">Update Point</button>
   <button type="button" id="submit-delete" class="btn btn-danger">Delete Point</button>
 </form>
 `);
   window.$modifyMarkerForm = $modifyMarkerForm;
+
+
+  $modifyMarkerForm.on("click", "#submit-update", function (e) {
+    if ($modifyMarkerForm[0].checkValidity()) {
+      e.preventDefault();
+      const mapId = $(this).closest('form').find('input').first().val();
+      const data = $(this).closest('form').serialize();
+      $.when(getMaps(`map_id=${mapId}`), updateMarker(data)).done((map) =>
+        createMap(map[0].maps[0])
+      );
+    } else {
+      alert('make sure to enter all fields with correct inputs');
+    }
+  });
+
+  $modifyMarkerForm.on("click", "#submit-delete", function (e) {
+    e.preventDefault();
+    const mapId = $(this).closest('form').find('input').first().val();
+    const id = $(this).closest('form').find('input').eq(3).val();
+    $.when(
+      getMaps(`map_id=${mapId}`),
+      deleteMarker(`id=${id}`)
+    ).done((map) => createMap(map[0].maps[0]));
+  });
+
 })
