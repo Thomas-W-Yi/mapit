ebe175d6e52030969d15dbaf7d5bd1aabd82015d
Borrisson <lucborris@gmail.com>
2021-02-10 12:16:33 +0000

    Change logic for list item creation

--- public/javascript/components/list_map.js
+++ public/javascript/components/list_map.js
@@ -7,25 +7,25 @@ $(() => {
     { isCreated = false, isFavorites = false, isContributed = false, currentMapId = false } = false
   ) {
 
-    let mapListItem = `<li id="${map.id}" class="list-group-item-action list-group-item-`
-    switch (map.id % 5) {
-      case (0):
-        mapListItem += `success`;
-        break;
-      case (1):
-        mapListItem += `info`;
-        break;
-      case (2):
-        mapListItem += `dark`;
-        break;
-      case (3):
-        mapListItem += `danger`;
-        break;
-      case (4):
-        mapListItem += `warning`;
-        break;
-    }
-    mapListItem += `">${map.name}</li>`;
+    let mapListItem = `<li id="${map.id}" class="mapLi list-group-item-action list-group-item-`;
+      switch (map.id % 5) {
+        case 0:
+          mapListItem += `success`;
+          break;
+        case 1:
+          mapListItem += `info`;
+          break;
+        case 2:
+          mapListItem += `dark`;
+          break;
+        case 3:
+          mapListItem += `danger`;
+          break;
+        case 4:
+          mapListItem += `warning`;
+          break;
+      }
+      mapListItem += `">${map.name}</li>`;
 
     return mapListItem;
   }
