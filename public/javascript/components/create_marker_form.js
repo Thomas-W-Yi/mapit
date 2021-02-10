48e66e5a91e238165eeb0b10c9d51215b144f25c
Borrisson <lucborris@gmail.com>
2021-02-10 16:31:29 +0000

    Add event listener for submit event of new marker form

--- public/javascript/components/create_marker_form.js
+++ public/javascript/components/create_marker_form.js
@@ -1,6 +1,13 @@
 $(() => {
   const $newMarkerForm = $(`
     <form id = 'new-marker-frm'>
+    <input type="hidden" name="map_id" value="">
+    <div class="form-group">
+    <input type="text" placeholder="Latitude" name = 'latitude' class="form-control" id="InputText" value="" readonly required>
+   </div>
+   <div class="form-group">
+    <input type="text" name='longitude' placeholder="Longitude" class="form-control" id="InputText" value="" readonly required>
+   </div>
     <div class="form-group">
     <input name="title" type="text" class="form-control" id="InputText"  placeholder="Enter Title" required>
     </div>
@@ -18,10 +25,15 @@ $(() => {
 
   $newMarkerForm.on("submit", function (e) {
     e.preventDefault();
-    $newMarkerForm.val("")
+    const mapId = $(this).find('input').first().val();
     let data = $(this).serialize();
-    data += `&map_id=${map.id}&latitude=${lat}&longitude=${lng}`;
-    addMarker(data).then(() => createMap(map));
+    console.log(data);
+    addMarker(data)
+    .then(() => getMaps(`map_id=${mapId}`))
+    .then(({maps}) =>
+    createMap(maps[0]))
+    ;
     views_manager.show('mapList');
   });
+
 })
