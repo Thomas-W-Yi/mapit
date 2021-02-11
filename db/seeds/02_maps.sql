8bffd66195d33b99296ca107903a1f29d743fef6
Rohit <rohitcricbuzz@gmail.com>
2021-02-11 18:56:09 +0000

    seeding of data in db files

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,2 +1,14 @@
 INSERT INTO maps (user_id, latitude, longitude, name) VALUES (1, 45.501690, -73.567253, 'Montreal Airports');
 INSERT INTO maps (user_id, latitude, longitude, name) VALUES (2, 43.653225, -79.383186, 'Toronto Airports');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (3, 51.049999, -114.066666, 'Calgary Parks');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (7, 50.445210, -104.618896, 'Regina Malls');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (9, 45.424721, -75.695000, 'Ottawa Schools');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (12, 43.466667, -80.516670, 'Waterloo Public Insitutions');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (3, 43.534787, -80.229381, 'Guelph Willow');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (2, 43.601960, -79.726319, 'Miss Com');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (11, 43.398008, -79.793951, 'Burlington');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (8, 43.231582, -79.837266, 'Hamilton');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (4, 43.665760, -79.898819, 'George');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (4, 43.766272, -79.749116, 'Brampton');
+INSERT INTO maps (user_id, latitude, longitude, name) VALUES (3, 43.545804, -80.259018, 'Guelph University');
+
--- db/seeds/04_favorites.sql
+++ db/seeds/04_favorites.sql
@@ -5,3 +5,52 @@ INSERT INTO favorites (user_id, map_id) VALUES (4, 2);
 INSERT INTO favorites (user_id, map_id) VALUES (6, 2);
 INSERT INTO favorites (user_id, map_id) VALUES (7, 1);
 INSERT INTO favorites (user_id, map_id) VALUES (8, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (1, 12);
+INSERT INTO favorites (user_id, map_id) VALUES (1, 7);
+INSERT INTO favorites (user_id, map_id) VALUES (1, 5);
+INSERT INTO favorites (user_id, map_id) VALUES (2, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (2, 4);
+INSERT INTO favorites (user_id, map_id) VALUES (2, 8);
+INSERT INTO favorites (user_id, map_id) VALUES (3, 5);
+INSERT INTO favorites (user_id, map_id) VALUES (3, 9);
+INSERT INTO favorites (user_id, map_id) VALUES (3, 8);
+INSERT INTO favorites (user_id, map_id) VALUES (3, 11);
+INSERT INTO favorites (user_id, map_id) VALUES (4, 6);
+INSERT INTO favorites (user_id, map_id) VALUES (4, 8);
+INSERT INTO favorites (user_id, map_id) VALUES (4, 7);
+INSERT INTO favorites (user_id, map_id) VALUES (4, 5);
+INSERT INTO favorites (user_id, map_id) VALUES (4, 13);
+INSERT INTO favorites (user_id, map_id) VALUES (4, 10);
+INSERT INTO favorites (user_id, map_id) VALUES (5, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (5, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (5, 7);
+INSERT INTO favorites (user_id, map_id) VALUES (5, 8);
+INSERT INTO favorites (user_id, map_id) VALUES (6, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (6, 8);
+INSERT INTO favorites (user_id, map_id) VALUES (6, 10);
+INSERT INTO favorites (user_id, map_id) VALUES (6, 7);
+INSERT INTO favorites (user_id, map_id) VALUES (6, 11);
+INSERT INTO favorites (user_id, map_id) VALUES (7, 4);
+INSERT INTO favorites (user_id, map_id) VALUES (7, 6);
+INSERT INTO favorites (user_id, map_id) VALUES (7, 9);
+INSERT INTO favorites (user_id, map_id) VALUES (7, 10);
+INSERT INTO favorites (user_id, map_id) VALUES (9, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (9, 3);
+INSERT INTO favorites (user_id, map_id) VALUES (9, 10);
+INSERT INTO favorites (user_id, map_id) VALUES (9, 11);
+INSERT INTO favorites (user_id, map_id) VALUES (9, 13);
+INSERT INTO favorites (user_id, map_id) VALUES (10, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (10, 3);
+INSERT INTO favorites (user_id, map_id) VALUES (10, 5);
+INSERT INTO favorites (user_id, map_id) VALUES (11, 2);
+INSERT INTO favorites (user_id, map_id) VALUES (11, 4);
+INSERT INTO favorites (user_id, map_id) VALUES (11, 6);
+INSERT INTO favorites (user_id, map_id) VALUES (12, 1);
+INSERT INTO favorites (user_id, map_id) VALUES (12, 6);
+INSERT INTO favorites (user_id, map_id) VALUES (12, 10);
+
+
+
+
+
+
